142047c6e09a06701a67163c168241374b4c49bd
marb61a <marb61a@gmail.com>
2018-08-23 11:35:30 +0100

    Updated index.js UI module

--- scripts/index.js
+++ scripts/index.js
@@ -22,4 +22,37 @@ const UI = (function(){
   // Hide menu
   const _hideMenu = () => menu.style.right = '-65%';
 
+  const _toggleHourlyWeather = () => {
+    let hourlyWeather = document.querySelector("#hourly-weather-wrapper"),
+        arrow = document.querySelector("#toggle-hourly-weather").children[0],
+        visible = hourlyWeather.getAttribute('visible'),
+        dailyWeather = document.querySelector('#daily-weather-wrapper');
+    
+    if(visible == 'false'){
+      hourlyWeather.setAttribute('visible', true);
+      hourlyWeather.style.bottom = 0;
+      arrow.style.transform = 'rotate(180deg)';
+      dailyWeather.style.opacity = 0;
+    } else if(visible == 'true'){
+      hourlyWeather.setAttribute('visible', false);
+      hourlyWeather.style.bottom = '-100%';
+      arrow.style.transform = 'rotate(0deg)';
+      dailyWeather.style.opacity = 1;
+    } else {
+      console.error("Unknown state of the hourly weather panel and visible attribute");
+    }
+  };
+
+  // Menu events
+  document.querySelector('#open-menu-btn').addEventListener('click', _showMenu);
+  document.querySelector('#close-menu-btn').addEventListener('click', _hideMenu);
+
+  // Hourly weather wrapper event
+  document.querySelector("#toggle-hourly-weather")
+    .addEventListener('click', _toggleHourlyWeather);
+
+  return {
+    showApp,
+    loadApp
+  }
 })();
