63339ed5fc8aa4ca2343c5c76bce34b641721126
marb61a <marb61a@gmail.com>
2018-08-26 11:19:37 +0100

    added localStorage API

--- scripts/index.js
+++ scripts/index.js
@@ -90,6 +90,42 @@ const UI = (function(){
   }
 })();
 
+
+/*
+  LocalStorage API
+*/
+const LOCALSTORAGE = (function(){
+  let savedCities = [];
+
+  const save = (city) => {
+    savedCities.push(city);
+    localStorage.setItem('savedCities', JSON.stringify('savedCities'));
+  };
+
+  const get = () => {
+    if(localStorage.getItem('savedCities') != null){
+      savedCities = JSON.parse(localStorage.getItem('savedCities'));
+    }
+  };
+
+  const remove = (index) => {
+    if(index < savedCities.length){
+      savedCities.splice(index, 1);
+      localStorage.setItem('savedCities', JSON.stringify(savedCities));
+    }
+  };
+
+  const getSavedCities = () => savedCities;
+
+  return {
+    save,
+    get,
+    remove,
+    getSavedCities
+  };
+})();
+
+
 /*
   Get Location Module
 */
